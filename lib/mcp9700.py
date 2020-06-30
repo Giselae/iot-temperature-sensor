ab13ca8069bf2334c1020cf9a358ee62871cc9ac
Gisela Dahlberg <gisda196@student.liu.se>
2020-06-30 20:14:46 +0200

    comments added

--- lib/mcp9700.py
+++ lib/mcp9700.py
@@ -1,4 +1,4 @@
-import time
+
 import machine
 import pycom
 
@@ -10,21 +10,31 @@ class MCP9700Result:
     error = VALID_INPUT
     degree_celsius = 0
 
+    #Constructor for the class that is created when a temperature is read 
     def __init__(self, error, degree_celsius):
         self.error = error
         self.degree_celsius = degree_celsius
 
+    #Method that confirmes a valid temperature value
     def input_is_valid (self):
         return self.error == MCP9700Result.VALID_INPUT
 
   
 class MCP9700:
 
+    #Constructor that's called in main when a object of MCP9700 is created
+    #machine.ADC() is called since the sensor is a analog sensor. 
     def __init__(self):
         adc = machine.ADC()
         self.analogTempPin = adc.channel(pin='P20')
         pycom.heartbeat(False)
 
+    #Method that reads value and print value to the terminal. If the sensor
+    #doesn't detect anything a object with error NO_INPUT is returned to main
+    #and the inner loop in main continues until the sensor detects a valid value. 
+    #If the sensor detects voltage it is converted to celsius, prints the voltage 
+    #and the degrees in celsius to the terminal and returns a object with the result 
+    #to main. 
     def read_print(self):
 
         milli_volts = self.analogTempPin.voltage()
@@ -36,8 +46,10 @@ class MCP9700:
             print(milli_volts)
             print(degree_celsius)
             return MCP9700Result(MCP9700Result.VALID_INPUT, degree_celsius)
-         
-    def colour_switcher(self, degree_celsius):
+    
+    # Method that switches the color of the LED on the Lopy4 depending
+    # of the temperature.      
+    def color_switcher(self, degree_celsius):
         
         if degree_celsius <=26.0:
             pycom.rgbled(0xe60099)
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-# main.py -- put your code here!
+# Import the libraries that are recuired
 import time
 import machine
 import pycom
-from mcp9700 import MCP9700, MCP9700Result
+#from mcp9700 import MCP9700, MCP9700Result
+import lib.mcp9700
 
+#Create instance of class MCP9700
 temp_sensor = MCP9700()
+
+#Reading the temperatur and sending data is placed in a while loop
+#since we want to do this repeaditly. First we create a variable which 
+#holds the temperature from the sensor. Then there's a inner while loop
+#that only executes if the sensor hasn't a valid value, otherwise the outer 
+# while loop continues and the data are send to Pybytes, the LED switches color 
+#and last the unit wait for 30 seconds until the loop continues. 
 
 while True:
     temp_value = temp_sensor.read_print()
@@ -12,7 +21,7 @@ while True:
         time.sleep(1)
         temp_value = temp_sensor.read_print()
     pybytes.send_signal(2, temp_value.degree_celsius)
-    temp_sensor.colour_switcher(temp_value.degree_celsius)
+    temp_sensor.color_switcher(temp_value.degree_celsius)
     time.sleep(2)
     pycom.heartbeat(False)
-    time.sleep(1)
+    time.sleep(30)
